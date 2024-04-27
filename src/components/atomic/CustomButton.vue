a18ef8465a32bacd7cb1b4605c2b729060875b32
cingfong <cingfong311@gmail.com>
2024-04-28 01:53:02 +0800

    fix: reinstall

---
+++ src/components/atomic/CustomButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+interface Props {
+  class?: string;
+}
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <!-- class="" -->
+  <button class="btn" :class="props.class">
+    <slot />
+  </button>
+</template>
+<style scope>
+.btn {
+  @apply text-white font-bold py-2 px-4 rounded;
+}
+</style>
