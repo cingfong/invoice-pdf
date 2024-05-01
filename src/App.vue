3472c34f147a63f1f0de99bf312110ff202dbfd9
cingfong <cingfong311@gmail.com>
2024-05-01 20:14:58 +0800

    feat: history modal file

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import { ref } from "vue";
 // import CustomButton from "./components/atomic/CustomButton.vue";
 import OrderTable from "./components/OrderTable.vue";
 import OrderForm from "./components/OrderForm.vue";
+import HistoryModal from "./components/HistoryModal.vue";
 
 const orderList = ref([]);
 </script>
 
 <template>
   <OrderForm />
+  <HistoryModal />
   <!-- <CustomButton :class="'bg-red-500'">生成 PDF</CustomButton> -->
   <OrderTable :order-list="orderList"></OrderTable>
 </template>
