bf4345c69db055dcb658f01f533c5c4d47b48d78
cingfong <cingfong311@gmail.com>
2024-06-29 16:22:47 +0800

    delete: 移除多餘檔案

--- src/composables/formData.ts
+++
@@ -1,9 +0,0 @@
-export interface formData {
-  name: string | null;
-  spec: string | null;
-  number: number | null;
-  unit: string | null;
-  price: number | null;
-  itemTotal: number | null;
-  remark: string | null;
-}
