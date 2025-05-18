9682d2c6ee77d09a70f04e698f13e3ad82a6da00
cingfong <cingfong>
2025-05-18 11:15:41 +0800

    feat: 更新 IndexTotal 組件，新增重置表單功能及調整樣式

--- components/index/IndexTotal.vue
+++ components/index/IndexTotal.vue
@@ -2,12 +2,14 @@
 import { useFormContext } from "#imports";
 import { computed } from "vue";
 import type { FormItem } from "~/constant/form";
+import { initialFormData } from "~/pages/index.vue";
 
-const { values: formValues } = useFormContext();
+const { values: formValues, resetForm } = useFormContext();
 
 const listTotal = computed(() => {
   return formValues.order_list.reduce(
-    (total: number, item: FormItem) => total + (Number(item.price) as number),
+    (total: number, item: FormItem) =>
+      total + (Number(item.price ?? 0) as number),
     0
   );
 });
@@ -20,45 +22,53 @@ const listTaxIncluded = computed(() => {
   return listTotal.value + listTax.value;
 });
 const cancelEdit = () => {
-  console.log("cancelEdit");
+  resetForm({
+    values: {
+      ...initialFormData,
+    },
+  });
 };
 </script>
 <template>
   <div
-    class="mt-5 p-4 bg-slate-50 rounded-xl shadow-md border border-slate-200"
+    class="mt-5 p-4 bg-slate-100 rounded-xl shadow-md border border-slate-200 backdrop-blur-sm"
   >
-    <!-- TODO: -->
-    <!-- v-if="editHistoryData" -->
-    <button
-      type="button"
-      class="w-full px-4 py-2.5 rounded-lg border-none text-sm font-medium cursor-pointer transition-all flex items-center gap-2 justify-center bg-slate-600 text-white hover:-translate-y-0.5 hover:shadow-md hover:bg-slate-700 active:translate-y-0 active:shadow-sm mb-3"
-      @click="cancelEdit"
+    <template v-if="formValues.id">
+      <button
+        type="button"
+        class="w-full px-4 py-2.5 rounded-lg border-none text-sm font-medium cursor-pointer transition-all flex items-center gap-2 justify-center bg-zinc-500 text-white hover:-translate-y-0.5 hover:shadow-md hover:bg-zinc-600 active:translate-y-0 active:shadow-sm mb-3"
+        @click="cancelEdit"
+      >
+        <UIcon name="i-heroicons-x-mark" class="w-4 h-4" />
+        取消編輯
+      </button>
+    </template>
+
+    <div
+      class="flex items-center gap-2 my-2 p-2 rounded-lg hover:bg-slate-50 transition-colors text-sm text-slate-700"
     >
-      <UIcon name="i-heroicons-x-mark" class="w-4 h-4" />
-      取消編輯
-    </button>
-
-    <div class="flex items-center gap-2 my-2 text-sm text-slate-700">
-      <UIcon name="i-heroicons-banknotes" class="w-5 h-5 text-red-600" />
+      <UIcon name="i-heroicons-banknotes" class="w-5 h-5 text-red-700" />
       <span>合計：</span>
-      <span class="font-semibold text-red-600">{{ listTotal }}</span>
-    </div>
-
-    <div class="flex items-center gap-2 my-2 text-sm text-slate-700">
-      <UIcon
-        name="i-heroicons-currency-dollar"
-        class="w-5 h-5 text-amber-600"
-      />
-      <span>稅金：</span>
-      <span class="font-semibold text-amber-600">{{ listTax }}</span>
+      <span class="font-semibold text-red-700">{{ listTotal }}</span>
     </div>
 
     <div
-      class="flex items-center gap-2 mt-3 pt-3 text-base font-semibold border-t border-slate-300 text-slate-800"
+      class="flex items-center gap-2 my-2 p-2 rounded-lg hover:bg-slate-50 transition-colors text-sm text-slate-700"
     >
-      <UIcon name="i-heroicons-banknotes" class="w-5 h-5 text-green-600" />
+      <UIcon
+        name="i-heroicons-currency-dollar"
+        class="w-5 h-5 text-amber-700"
+      />
+      <span>稅金：</span>
+      <span class="font-semibold text-amber-700">{{ listTax }}</span>
+    </div>
+
+    <div
+      class="flex items-center gap-2 mt-3 pt-3 p-2 rounded-lg text-base font-semibold border-t border-slate-300 text-slate-800 hover:bg-slate-50 transition-colors"
+    >
+      <UIcon name="i-heroicons-banknotes" class="w-5 h-5 text-green-700" />
       <span>總計：</span>
-      <span class="font-semibold text-green-600">{{ listTaxIncluded }}</span>
+      <span class="font-semibold text-green-700">{{ listTaxIncluded }}</span>
     </div>
   </div>
 </template>
