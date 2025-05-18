2d7ad3865cc05b94fb9eebf208989754b523798d
cingfong <cingfong>
2025-05-18 11:17:58 +0800

    feat: 新增刪除訂單和歷史記錄的 API，並定義表單項目類型

---
+++ server/api/deleteOrder.delete.ts
@@ -0,0 +1,39 @@
+import { defineEventHandler, createError, readBody, getCookie } from 'h3'
+import { pool } from '@/server/utils/db'
+import type { FormItem } from '~/constant/form';
+import { COOKIE_KEY } from '~/constant/cookie';
+
+export type FormOrderBody = {
+  id: number;
+  updated_at: string;
+  order_list: FormItem[];
+  order_title: string;
+  token: string;
+  disabled:boolean;
+  user_id:number;
+  order_type:boolean;
+}
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event) as FormOrderBody
+  const token = getCookie(event, COOKIE_KEY.TOKEN);
+  
+  try {
+    const result = await pool.query('DELETE FROM "history_order" WHERE "id" = $1 AND "token" = $2', [body.id, token])
+    
+    if (result.rowCount === 0) {
+      throw createError({
+        statusCode: 403,
+        message: '無權限刪除此訂單或訂單不存在'
+      })
+    }
+    
+    return result.rows
+  } catch (error) {
+    console.error(error)
+    throw createError({
+      statusCode: 500,
+      message: '連線錯誤'
+    })
+  }
+})
